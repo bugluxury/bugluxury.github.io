017de00b5e47e6e1361a52f6b429c5bb48892c9f
wuliY <stu006@qq.com>
2016-04-13 15:13:38 +0800

    init

---
+++ temp菁英/js/get.php
@@ -0,0 +1,1 @@
+(function(){if(document.getElementById("gt_lib")){return};document.write('<script type="text/javascript" charset="utf-8" id="gt_lib" src="https://static.geetest.com/static/js/geetest.5.3.6.js"></script>')})()
